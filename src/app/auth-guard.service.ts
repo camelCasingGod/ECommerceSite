7b2b8228ecb131f3aded392687d2394a207a363c
camelCasingGod <57607665+camelCasingGod@users.noreply.github.com>
2020-06-28 17:33:31 -0700

    Redirecting After Log-In

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,4 +1,4 @@
-import { Router } from '@angular/router';
+import { Router, RouterStateSnapshot } from '@angular/router';
 import { CanActivate } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
@@ -11,11 +11,11 @@ export class AuthGuard implements CanActivate {
 
   constructor(private auth: AuthService, private router: Router) { }
 
-  canActivate() {
+  canActivate(route, state: RouterStateSnapshot) {
     return this.auth.user$.map(user => {
       if (user) return true;
 
-      this.router.navigate(['/login']);
+      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
       return false;
     });
   }
