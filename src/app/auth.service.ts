559784d2fed064051a591f20118e4beffa6e8d48
camelCasingGod <57607665+camelCasingGod@users.noreply.github.com>
2020-06-30 12:04:10 -0700

    Fixed the Logout Bug

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -3,6 +3,11 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase';
 import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
+import { AppUser } from './models/app-user';
+import { switchMap } from 'rxjs/operators';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/observable/of'
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +16,7 @@ export class AuthService {
 
   user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) {
+  constructor(private userService: UserService, private afAuth: AngularFireAuth, private route: ActivatedRoute) {
     this.user$ = afAuth.authState;
   }
 
@@ -24,5 +29,15 @@ export class AuthService {
 
   logout() {
     this.afAuth.signOut();
+  }
+
+  get appUser$() : Observable<AppUser> {
+
+    return this.user$
+      .pipe(switchMap(user => {
+        if (user) return this.userService.get(user.uid).valueChanges()
+
+        return Observable.of(null);
+      }));
   }
 }
