7a61d025207cda6abfa1a8fd125a2fa84d7c2ac8
camelCasingGod <57607665+camelCasingGod@users.noreply.github.com>
2020-07-02 12:39:56 -0700

    Adding Custom Validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,7 @@ import { ProductFormComponent } from './admin/product-form/product-form.componen
 import { CategoryService } from './category.service';
 import { FormsModule } from '@angular/forms';
 import { ProductService } from './product.service';
+import { CustomFormsModule } from 'ngx-custom-validators';
 
 @NgModule({
   declarations: [
@@ -48,6 +49,8 @@ import { ProductService } from './product.service';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    CustomFormsModule,
+
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
